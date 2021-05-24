56b6a2c039454ffe4ef8d54f16832b28a013d026
Щербатых Антон Евгеньевич <tai-prg3@tai-prg3-apc.bnpp.pvt>
2021-05-24 12:51:22 +0500

    оформил фикс.математику по хэдеру и исполнению

--- fixmath.cpp
+++ fixmath.cpp
@@ -1,2 +1,49 @@
 #include        "fixmath.h"
 
+const float FIXED2FLOAT_COEFF = 1.0f / 65536.0f;
+
+inline fixed_t int2fixed_t(int16_t x)
+{
+ return(((int32_t)x)<<16);
+}
+
+inline fixed_t float2fixed_t(float x)
+{
+ return((fixed_t)(65536.0f*x));
+}
+
+inline int32_t fraction2fixed_t(int32_t a,int32_t b)
+{
+ return((int32_t)((((int32_t)a)<<16)/(int32_t)b));
+}
+
+inline fixed_t fixdiv(fixed_t a,fixed_t b)
+{
+ return((a<<16)/b);
+}
+
+inline fixed_t fixmul(fixed_t a,fixed_t b)
+{
+ return((a>>16)*b);
+}
+
+inline int32_t fixed2int(fixed_t x)
+{
+ return((int32_t)(x>>16));
+}
+
+inline float fixed2float(fixed_t x)
+{
+ return(((float)x) * 65536.0f);
+}
+
+inline fixed_t fixabs(fixed_t x)
+{
+ if (x<0) return(-x);
+ return(x);
+}
+
+inline fixed_t frac(fixed_t x)
+{
+ return(x&0xFFFFl);
+}
